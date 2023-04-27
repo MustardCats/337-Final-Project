50cef7e0a090da15c21a91d65f2b9caac524a2df
cat-w-101 <catwong2023@arizona.edu>
2023-04-27 16:42:07 -0700

    sprite code updated with new sprites

--- public_html/sprites.js
+++ public_html/sprites.js
@@ -4,36 +4,82 @@
 
 const atlasData = {
     frames: {
-        kirbyleft: {
+        enemy1left{
             frame: { x: 0, y: 0, w: 32, h: 32 },
             sourceSize: { w: 32, h: 32 },
             spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
         },
-        kirbyright: {
+        enemy1right{
             frame: { x: 32, y: 0, w: 32, h: 32 },
             sourceSize: { w: 32, h: 32 },
             spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
         },
-        powerup1: {
+        enemy2left{
             frame: { x: 64, y: 0, w: 32, h: 32 },
             sourceSize: { w: 32, h: 32 },
             spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
         },
-        grass: {
+        powerup2: {
+            frame: { x: 96, y: 0, w: 32, h: 32 },
+            sourceSize: { w: 32, h: 32 },
+            spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
+        },
+        enemy2right{
             frame: { x: 0, y: 32, w: 32, h: 32 },
             sourceSize: { w: 32, h: 32 },
             spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
         },
-        powerup2: {
+        enemy3left{
             frame: { x: 32, y: 32, w: 32, h: 32 },
+            sourceSize: { w: 32, h: 32 },
+            spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
+        },
+        enemy3right{
+            frame: { x: 64, y: 32, w: 32, h: 32 },
+            sourceSize: { w: 32, h: 32 },
+            spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
+        },
+        powerup1: {
+            frame: { x: 96, y: 32, w: 32, h: 32 },
+            sourceSize: { w: 32, h: 32 },
+            spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
+        },
+        kirbyleft: {
+            frame: { x: 0, y: 64, w: 32, h: 32 },
+            sourceSize: { w: 32, h: 32 },
+            spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
+        },
+        kirbyright: {
+            frame: { x: 32, y: 64, w: 32, h: 32 },
+            sourceSize: { w: 32, h: 32 },
+            spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
+        },
+        grass: {
+            frame: { x: 64, y: 64, w: 32, h: 32 },
+            sourceSize: { w: 32, h: 32 },
+            spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
+        },
+        shake1{
+            frame: { x: 96, y: 64, w: 32, h: 32 },
+            sourceSize: { w: 32, h: 32 },
+            spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
+        },
+
+        shake2{
+            frame: { x: 0, y: 96, w: 32, h: 32 },
+            sourceSize: { w: 32, h: 32 },
+            spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
+        },
+        shake3{
+            frame: { x: 32, y: 96, w: 32, h: 32 },
             sourceSize: { w: 32, h: 32 },
             spriteSourceSize: { x: 0, y: 0, w: 32, h: 32 }
         }
     },
     meta: {
-        image: 'sprites/spritesheet.png',
+        image: 'sprites/new_spritesheet.png',
         format: 'RGBA8888',
-        size: { w: 96, h: 64 },
+        size: { w: 128, h: 128 },
         scale: 1
     }
 }
